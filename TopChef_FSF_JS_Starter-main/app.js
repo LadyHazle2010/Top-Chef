6414e903c795abaa4613aa0ef484c9d34ab34523
T Jackson <sweetladyhazle@gmail.com>
2023-03-24 20:31:10 -0500

    Top-Chef is completed

--- TopChef_FSF_JS_Starter-main/app.js
+++ TopChef_FSF_JS_Starter-main/app.js
@@ -118,12 +118,12 @@ let todaysSpecialDish = {
     cuisine: "Mexican",
     servings: 4,
     ingredients: ["corn", "cheese"],
-}
+};
 
 // <<<<<<<<<<<<<<<<< EXAMPLE FUNCTION <<<<<<<<<<<<<<<<<
 
 function findMexicanFood(allDishes) {
-    alert("Searching for Mexican dishes...")
+    alert("Searching for Mexican dishes...");
     let results = allDishes.filter(function (el) {
         if (el.cuisine === "Mexican") {
             return true;
@@ -132,37 +132,77 @@ function findMexicanFood(allDishes) {
         }
     });
     // TODO #1: Step through this function with the debugger to get a better understanding of the structure of this function
-
-    // Debug tip: Place a Breakpoint on the alert AFTER the filter, then hover over "results" to see results of filter 
+    // Debug tip: Place a Breakpoint on the alert AFTER the filter, then hover over "results" to see results of filter
     // without having to step through the whole thing!
-    alert("Found all Mexican dishes!  Check the console for full output.")
-    return results
+    alert("Found all Mexican dishes!  Check the console for full output.");
+    return results;
 }
 
 // <<<<<<<<<<<<<<<<< MAP & FILTER FUNCTIONS <<<<<<<<<<<<<<<<<
 
 function findItalianFood(allDishes) {
-    alert("Searching for Italian dishes...")
+    alert("Searching for Italian dishes...");
     // TODO #2: Filter all dishes for those that have a cuisine type of Italian
-    alert("Found all Italian dishes!  Check the console for full output")
+    let results = allDishes.filter(function (el) {
+        return el.cuisine === "Italian";
+    });
+    alert("Found all Italian dishes!  Check the console for full output");
+    return results;
 }
 
 function searchCuisines(allDishes) {
-    alert("Searching for dishes by cuisine...")
+    alert("Searching for dishes by cuisine...");
     // TODO #3: Gather user input for a cuisine to search for, then filter for all dishes matching this cuisine type
-    alert("Found all dishes matching the cuisine search term!  Check the console for full output")
+    let cuisineType = customPrompt("Search for dishes by cuisine", [
+        "Italian",
+        "Mexican",
+        "French",
+        "Irish",
+        "Vegetarian",
+        "Hungarian",
+    ]);
+    let results = allDishes.filter(function (el) {
+        return el.cuisine === cuisineType;
+    });
+    alert(
+        "Found all dishes matching the cuisine search term!  Check the console for full output"
+    );
+    return results;
 }
 
 function searchIngredients(allDishes) {
-    alert("Searching for dishes by ingredient...")
+    alert("Searching for dishes by ingredient...");
     // TODO #4: Gather user input for an ingredient to search for, then filter for all dishes that INCLUDE this ingredient in their ingredients array property
-    alert("Found all dishes that contain the ingredient search term!  Check the console for full output")
+    let ingredient = customPrompt("Search for dishes by ingredient", [
+        "tomato",
+        "cheese",
+        "corn",
+        "flour",
+        "sugar",
+        "beef",
+        "cabbage",
+        "chickpea",
+        "parsley",
+    ]);
+    let results = allDishes.filter(function (el) {
+        return el.ingredients.includes(ingredient);
+    });
+    alert(
+        "Found all dishes that contain the ingredient search term!  Check the console for full output"
+    );
+    return results;
 }
 
 function generateCuisineDishName(allDishes) {
-    alert("Combining cuisine and dish names...")
+    alert("Combining cuisine and dish names...");
     // TODO #5: Apply the concatenatorFunction to each dish in allDishes, then log to the console the modified result
-    alert("Successfully combined cuisine and dish names!  Check the console for full output.")
+    let result = allDishes.map(function (el) {
+        return el.cuisine + " " + el.name;
+    });
+    alert(
+        "Successfully combined cuisine and dish names!  Check the console for full output."
+    );
+    return result;
 }
 
 // <<<<<<<<<<<<<<<<< EMAIL AND TEXT MARKETING MESSAGES <<<<<<<<<<<<<<<<<
@@ -175,7 +215,7 @@ function emailMessage(dishOfTheDay) {
     Thank you for subscribing to email alert messages!
     Today's Dish of the day is:
 
-    <DISH OF THE DAY HERE>
+    ${formatSpecialDish(dishOfTheDay)}
 
     We hope to see you in soon!
 
@@ -196,7 +236,7 @@ function textMessage(dishOfTheDay) {
     This is an automated text message alert.
     Today's Dish of the day is:
 
-    <DISH OF THE DAY HERE>
+    ${formatSpecialDish(dishOfTheDay)}
 
     We hope to see you in soon!
 
@@ -210,71 +250,115 @@ function textMessage(dishOfTheDay) {
 }
 
 function generateMarketingMessage(dishOfTheDay, messageTypeCallback) {
-    alert('Sending final message to all 389 customers...')
+    alert("Sending final message to all 389 customers...");
     // TODO #7: Call the passed-in callback function on the dishOfTheDay.  Save the result as a variable
+    let result = messageTypeCallback(dishOfTheDay);
     // Then, log that result to the console
-    alert('Success!  Check the console for a copy of the final marketing message!')
+    console.log(result);
+    alert(
+        "Success!  Check the console for a copy of the final marketing message!"
+    );
 }
 
 // <<<<<<<<<<<<<<<<< CUSTOM PROMPT FUNCTION <<<<<<<<<<<<<<<<<
 
 function customPrompt(promptQuestion, arrayOfValidResponses) {
     // TODO #10: Replace all instances of prompt() in the above functions with customPrompt()
-    let response
+    let response;
     while (!arrayOfValidResponses.includes(response)) {
-        response = prompt(promptQuestion)
+        response = prompt(promptQuestion);
     }
-    return response
+    return response;
 }
 
 // <<<<<<<<<<<<<<<<< MAIN MENU FUNCTION <<<<<<<<<<<<<<<<<
 
 function runApp(allDishes, specialDish) {
-    alert("Welcome to the Recipe Searching Application!")
-    let userChoice = customPrompt(`Press 1 to search for Mexican dishes.
+    alert("Welcome to the Recipe Searching Application!");
+    let userChoice = customPrompt(
+        `Press 1 to search for Mexican dishes.
     Press 2 to search for Italian dishes.    
     Press 3 to search for dishes by cuisine.
     Press 4 to search for dishes by ingredient.
     Press 5 to see a list of cuisines & dish names.
     Press 6 to send a marketing text message for Today's Special Dish.
     Press 7 to send a marketing email message for Today's Special Dish.
-    Enter "Exit" to quit the application.`, ["1", "2", "3", "4", "5", "6", "7", "Exit"])
+    Enter "Sum" to calcultate total servicing count for all dishes.
+    Enter "Exit" to quit the application.`,
+        ["1", "2", "3", "4", "5", "6", "7", "Sum", "Exit"]
+    );
     switch (userChoice) {
         case "1":
-            let mexicanDishes = findMexicanFood(allDishes)
-            console.log(mexicanDishes)
-            break
+            let mexicanDishes = findMexicanFood(allDishes);
+            displaySearchResults(mexicanDishes);
+            break;
         case "2":
-            let italianDishes = findItalianFood(allDishes)
-            console.log(italianDishes)
-            break
+            let italianDishes = findItalianFood(allDishes);
+            displaySearchResults(italianDishes);
+            break;
         case "3":
-            let cuisineSearchResults = searchCuisines(allDishes)
-            console.log(cuisineSearchResults)
-            break
+            let cuisineSearchResults = searchCuisines(allDishes);
+            displaySearchResults(cuisineSearchResults);
+            break;
         case "4":
-            let ingredientSearchResults = searchIngredients(allDishes)
-            console.log(ingredientSearchResults)
-            break
+            let ingredientSearchResults = searchIngredients(allDishes);
+            displaySearchResults(ingredientSearchResults);
+            break;
         case "5":
-            let concatenatedDishes = generateCuisineDishName(allDishes)
-            console.log(concatenatedDishes)
-            break
+            let concatenatedDishes = generateCuisineDishName(allDishes);
+            console.log(concatenatedDishes);
+            break;
         case "6":
-            // TODO #8: Call the appropriate function to generate the marketing text message.  
+            // TODO #8: Call the appropriate function to generate the marketing text message.
             // You will need to provide today's dish and the appropriate callback function as arguments!
-            break
+            generateMarketingMessage(specialDish, textMessage);
+            break;
         case "7":
-            // TODO #9: Call the appropriate function to generate the marketing email message.  
+            // TODO #9: Call the appropriate function to generate the marketing email message.
             // You will need to provide today's dish and the appropriate callback function as arguments!
-            break
+            generateMarketingMessage(specialDish, emailMessage);
+            break;
+        case "Sum":
+            alert("Today's Total Serving Count");
+            calculateTotalServingCount(allDishes);
+            break;
         case "Exit":
-            alert("Thank you for using the Recipe Searching Application!  Goodbye!")
-            return
+            alert(
+                "Thank you for using the Recipe Searching Application!  Goodbye!"
+            );
+            return;
         default:
-            alert("Invalid choice, please try your selection again!")
-            return runApp(allDishes, specialDish)
+            alert("Invalid choice, please try your selection again!");
+            return runApp(allDishes, specialDish);
     }
 }
 
-runApp(dishes, todaysSpecialDish)
+// <<<<<<<<<<<<<<<<< All Information of Single Dish <<<<<<<<<<<<<<<<<
+
+function formatSpecialDish(specialDish) {
+    let result = JSON.stringify(specialDish, null, "\t");
+    return result;
+}
+
+// <<<<<<<<<<<<<<<<< Information of All Dishes <<<<<<<<<<<<<<<<<
+
+function displaySearchResults(searchResults) {
+    let resultArray = searchResults.map(function (el) {
+        return formatSpecialDish(el);
+    });
+
+    let result = resultArray.join(", ");
+    console.log(result);
+}
+
+// <<<<<<<<<<<<<<<<< Calculate Total Serving Count <<<<<<<<<<<<<<<<<
+
+function calculateTotalServingCount(dishes) {
+    let servingArray = dishes.map(function (el) {
+        return el.servings;
+    });
+    let sum = servingArray.reduce((total, item) => total + item);
+    console.log(sum);
+    return sum;
+}
+runApp(dishes, todaysSpecialDish);
